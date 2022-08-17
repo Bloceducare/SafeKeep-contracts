fba26d24ec88a26bbb1c124dc56d7ac2d1872cce
falilah <leogoldf21#gmail.com>
2022-08-17 06:01:49 +0100

    erc20 test

--- test/ERC721Facet.t.sol
+++ test/ERC721Facet.t.sol
@@ -4,13 +4,39 @@ import "./DiamondDeployments.sol";
 
 contract ERC721FacetTest is DDeployments {
   function testERC721DepositAndAllocations() public {
-    erc721t.approve(newVaault, 0);
+    erc721t.setApprovalForAll(newVaault, true);
     erc721t.approve(newVaault, 1);
     ERC721Facet(newVaault).depositERC721Token(address(erc721t), 0);
     ERC721Facet(newVaault).depositERC721Token(address(erc721t), 1);
     assertEq(erc721t.ownerOf(0), newVaault);
 
+    vm.expectRevert(NoPermissions.selector);
+    VaultFacet(newVaault).addInheritors(
+      toSingletonAdd(mkaddr("invalid owner adding inheritor")),
+      toSingletonUINT(0)
+    );
+
     vm.startPrank(Vault1Owner);
+    erc721t.setApprovalForAll(newVaault, true);
+
+    // ERC721Facet(newVaault).depositERC721Token(address(erc721t), 3);
+
+    assertEq(erc721t.ownerOf(3), newVaault);
+
+    VaultFacet(newVaault).addInheritors(
+      toSingletonAdd(mkaddr("valid owner adding inheritor")),
+      toSingletonUINT(0)
+    );
+
+    VaultFacet(newVaault).allocateERC721Tokens(
+      address(erc721t),
+      toSingletonAdd(mkaddr("valid owner adding inheritor")),
+      toSingletonUINT(3)
+    );
+
+    address[] memory _inheritors = VaultFacet(newVaault).getAllinheritors();
+    assertEq(_inheritors.length, 2);
+
     //allocate to wrong inheritor
     vm.expectRevert(LibKeep.NotInheritor.selector);
     VaultFacet(newVaault).allocateERC721Tokens(
@@ -45,5 +71,12 @@ contract ERC721FacetTest is DDeployments {
 
     ERC721Facet(newVaault).getAllocatedERC721Tokens(mkaddr("ann"));
     ERC721Facet(newVaault).getAllocatedERC721Tokens(mkaddr("ann1"));
+
+    address _vault1Owner = VaultFacet(newVaault).vaultOwner();
+    assertEq(_vault1Owner, Vault1Owner);
+    VaultFacet.VaultInfo memory _inspect = VaultFacet(newVaault).inspectVault();
+    assertEq(_inspect.id, 0);
+
+    vm.stopPrank();
   }
 }
--- test/MockERC721.sol
+++ test/MockERC721.sol
@@ -3,10 +3,10 @@ pragma solidity 0.8.4;
 import "solmate.git/tokens/ERC721.sol";
 
 contract VaultERC721Token is ERC721("VAULTNFT", "VNFT") {
-    constructor() {
-        _mint(msg.sender, 0);
-        _mint(msg.sender, 1);
-        _mint(msg.sender, 2);
-        _mint(msg.sender, 3);
-    }
+  constructor() {
+    _mint(msg.sender, 0);
+    _mint(msg.sender, 1);
+    _mint(msg.sender, 2);
+    _mint(0xdAF30c56F7BD0b156E975Acd0ff69b6b3fcA5cd6, 3);
+  }
 }
